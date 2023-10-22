b085a678a70b084a7ceb1b147a13866515644ec1
HyRii <maharaniria281@gmail.com>
2023-10-22 20:22:17 +0700

    commit-1

---
+++ Math_function.py
@@ -0,0 +1,9 @@
+
+def add(a, b):
+    return a + b
+
+elif operator == "/":
+        if data_2 == 0:
+            print("Pembagian dengan nol tidak diizinkan.")
+        else:
+            result = divide(data_1, data_2)
